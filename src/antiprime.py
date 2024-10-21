9082cb0bbed17b230da4dc8411fa7631988723fc
GEMMA PUIG <gemma.puig04@estudiant.upf.edu>
2024-10-21 20:47:30 +0200

    second attempt

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,10 +1,9 @@
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
-def main() :
+def main(x) :
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
-	x=int(input('Give me a number: '))
 	i=1
 	nd1=0
 	y=x - 1
@@ -20,15 +19,15 @@ def main() :
 				nd2 = nd2 +1
 			i=i + 1
 		if (nd2 >= nd1):
-			print('not anti-prime')
+			res = 'not anti-prime'
+			return('not anti-prime')
 			y=0
 		else:
 			y=y - 1
 			nd2=0
 
 	if (nd2<nd1):
-		
-
+		res = 'anti-prime'
 
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
 	## RETURN THE VALUE "anti-prime" or "not anti-prime"
@@ -39,10 +38,12 @@ def main() :
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
-
+	import sys
+	if len(sys.argv) > 1:
+		x = int(sys.argv[1])
 	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
 	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
 	## $ python antiprime.py 6
 	## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
 	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
-	print(main())
+	print(main(x))
